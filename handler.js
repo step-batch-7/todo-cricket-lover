c5d1b3b7e208a2e7aaf4c54371c8c3ca23391ba1
phaneendra <phaneendraandukuri@gmail.com>
2020-02-05 15:11:07 +0530

    added feature: new tasks can be added now

--- handler.js
+++ handler.js
@@ -1,6 +1,8 @@
 'use strict';
 const fs = require('fs');
+const querystring = require('querystring');
 const { App } = require('./app');
+const COMMENTS_PATH = `${__dirname}/data/todo.json`;
 
 const CONTENT_TYPES = require('./public/lib/mimeTypes');
 
@@ -12,16 +14,31 @@ const serveBadRequestPage = function(req, res) {
 const STATIC_FOLDER = `${__dirname}/public`;
 
 const serveStaticPage = function(req, res, next) {
-	const filename = req.url === '/' ? '/index.html' : req.url;
-	
+  const filename = req.url === '/' ? '/index.html' : req.url;
+
   if (!fs.existsSync(`./public${filename}`)) {
-		return next();
+    return next();
   }
   let fileContent = fs.readFileSync(`${STATIC_FOLDER}${filename}`);
   const [, extension] = filename.split('.');
-  
   res.setHeader('Content-Type', CONTENT_TYPES[extension]);
   res.end(fileContent);
+};
+
+const todos = [];
+
+const createNewTask = function(req, res) {
+  const todo = querystring.parse(req.body);
+  todo.items = todo.items.map((item, index) => {
+    return { item, id: index + 1 };
+  });
+  todo['id'] = todos.length + 1;
+  todos.push(todo);
+
+  fs.writeFileSync(COMMENTS_PATH, JSON.stringify(todos));
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(todos));
 };
 
 const readBody = function(req, res, next) {
@@ -45,6 +62,7 @@ const app = new App();
 
 app.use(readBody);
 app.get('', serveStaticPage);
+app.post('createNewTask', createNewTask);
 app.get('', serveBadRequestPage);
 app.post('', serveBadRequestPage);
 app.use(methodNotAllowed);
