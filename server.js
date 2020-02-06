068d206cc6933b94a80ac420db56f3e9c169f91d
phaneendra <phaneendraandukuri@gmail.com>
2020-02-06 14:08:57 +0530

    resolved some eslint errors

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 'use strict';
-const {Server} = require('http');
-const {app} = require('./handler');
+const { Server } = require('http');
+const { app } = require('./handler');
 
 const main = function() {
+  const port = 9000;
   const server = new Server(app.serve.bind(app));
   server.on('listening', () => console.warn('server is listening'));
-  server.listen(9000);
+  server.listen(port);
 };
 main();
