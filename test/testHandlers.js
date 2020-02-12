1d3964adfa3ddc2d5a230b029fc6973f47c8d699
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-02-12 19:06:31 +0530

    [phani | bernie] modified test structure in testHandlers

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -3,22 +3,27 @@ const request = require('supertest');
 const fs = require('fs');
 const { app } = require('../lib/handler');
 
-describe('GET request', function() {
-  it('should get index.html when the path is /', function(done) {
-    request(app.serve.bind(app))
-      .get('/')
-      .expect('Content-Type', 'text/html', done)
-      .expect(/Todo/)
-      .expect(200);
+describe('GET', function() {
+  context('/', function() {
+    it('should get index.html when the path is /', function(done) {
+      request(app.serve.bind(app))
+        .get('/')
+        .expect('Content-Type', 'text/html', done)
+        .expect(/Todo/)
+        .expect(200);
+    });
   });
-  it('should give not found when incorrect path is given', function(done) {
-    request(app.serve.bind(app))
-      .get('/badFile')
-      .expect(404, done);
+
+  context('/badFile', function() {
+    it('should give not found when incorrect path is given', function(done) {
+      request(app.serve.bind(app))
+        .get('/badFile')
+        .expect(404, done);
+    });
   });
 });
 
-describe('POST todo', function() {
+describe('POST', function() {
   beforeEach(function() {
     sinon.replace(fs, 'writeFileSync', () => {});
   });
@@ -26,57 +31,71 @@ describe('POST todo', function() {
     sinon.restore();
   });
 
-  it('should create new todo and post on index page', function(done) {
-    request(app.serve.bind(app))
-      .post('/createNewTodo')
-      .send('title=phani')
-      .expect(200, done);
+  context('/createNewTodo', function() {
+    it('should create new todo and post on index page', function(done) {
+      request(app.serve.bind(app))
+        .post('/createNewTodo')
+        .send('title=phani')
+        .expect(200, done);
+    });
   });
 
-  it('should delete todo from index page', function(done) {
-    request(app.serve.bind(app))
-      .post('/deleteTodo')
-      .send('todoId=1')
-      .expect(200, done);
+  context('/deleteTodo', function() {
+    it('should delete todo from index page', function(done) {
+      request(app.serve.bind(app))
+        .post('')
+        .send('todoId=1')
+        .expect(200, done);
+    });
   });
 
-  it('should create new item in a todo and post on index page', function(done) {
-    request(app.serve.bind(app))
-      .post('/createNewItem')
-      .send('item=picture&todoId=2')
-      .expect(200, done);
+  context('/createNewItem', function() {
+    it('should create new item in a todo and post on index page', function(done) {
+      request(app.serve.bind(app))
+        .post('/createNewItem')
+        .send('item=picture&todoId=2')
+        .expect(200, done);
+    });
   });
 
-  it('should delete item in a todo from index page', function(done) {
-    request(app.serve.bind(app))
-      .post('/deleteItem')
-      .send('taskId=1&todoId=2')
-      .expect(200, done);
+  context('/deleteItem', function() {
+    it('should delete item in a todo from index page', function(done) {
+      request(app.serve.bind(app))
+        .post('/deleteItem')
+        .send('taskId=1&todoId=2')
+        .expect(200, done);
+    });
   });
 
-  it('should change status of an item in a todo from index page', function(done) {
-    request(app.serve.bind(app))
-      .post('/changeItemStatus')
-      .send('taskId=1&todoId=2')
-      .expect(200, done);
+  context('/changeItemStatus', function() {
+    it('should change status of an item in a todo from index page', function(done) {
+      request(app.serve.bind(app))
+        .post('/changeItemStatus')
+        .send('taskId=1&todoId=2')
+        .expect(200, done);
+    });
   });
 
-  it('should rename the title of a todo', function(done) {
-    request(app.serve.bind(app))
-      .post('/renameTitle')
-      .send('todoId=2&newTitle=phani')
-      .expect(200, done);
+  context('/renameTitle', function() {
+    it('should rename the title of a todo', function(done) {
+      request(app.serve.bind(app))
+        .post('/renameTitle')
+        .send('todoId=2&newTitle=phani')
+        .expect(200, done);
+    });
   });
 
-  it('should modify the item of a todo', function(done) {
-    request(app.serve.bind(app))
-      .post('/modifyItem')
-      .send('todoId=2&newItem=phani&taskId=1')
-      .expect(200, done);
+  context('/modifyItem', function() {
+    it('should modify the item of a todo', function(done) {
+      request(app.serve.bind(app))
+        .post('/modifyItem')
+        .send('todoId=2&newItem=phani&taskId=1')
+        .expect(200, done);
+    });
   });
 });
 
-describe('PUT request', function() {
+describe('PUT /', function() {
   it('should give method not allowed when wrong method is asked', function(done) {
     request(app.serve.bind(app))
       .put('/')
